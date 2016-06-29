090a28409cc2beff57989486b12e870f31d11f96
petroslikidis <petros.likidis@gmail.com>
2016-06-29 13:49:15 +0000

    Test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BasicWebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page. Hakim!!!!";
 
             return View();
         }
